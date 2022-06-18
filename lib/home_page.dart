6834552c1bac68d59e37f09aeda922fa2b5f7822
MdAsifUrRahmanAbir <mdasifurrahmanabir@gmail.com>
2022-06-18 11:36:51 +0600

    weather app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,6 @@ import 'package:jiffy/jiffy.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -34,31 +33,17 @@ class _HomePageState extends State<HomePage> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              Text(
-                                "${Jiffy(DateTime.now()).format("MMM do yy")}, ${Jiffy(DateTime.now()).format("h:mm")}",
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.black),
+                              Text("${Jiffy(DateTime.now()).format("MMM do yy")}, ${Jiffy(DateTime.now()).format("h:mm")}",
+                                style: const TextStyle(fontSize: 16, color: Colors.black),
                               ),
-                              Text(
-                                "${weatherMap!["name"]}",
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.black),
-                              ),
+                              Text("${weatherMap!["name"]}", style: const TextStyle(fontSize: 16, color: Colors.black),),
                             ],
                           )),
                       Center(
                         child: Column(
                           children: [
-                            Image.network(
-                              "https://th.bing.com/th/id/OIP.KOhzhOJQ4AHmN25RM2zFbAHaFP?pid=ImgDet&rs=1",
-                              height: 90,
-                              width: 100,
-                            ),
-                            Text(
-                              "${forecastMap!["list"][0]["main"]["temp"]}°",
-                              style:
-                                  TextStyle(fontSize: 50, color: Colors.black),
-                            ),
+                            Image.network("https://th.bing.com/th/id/OIP.KOhzhOJQ4AHmN25RM2zFbAHaFP?pid=ImgDet&rs=1", height: 90, width: 100,),
+                            Text("${forecastMap!["list"][0]["main"]["temp"]}°", style: const TextStyle(fontSize: 50, color: Colors.black),),
                           ],
                         ),
                       ),
@@ -67,36 +52,20 @@ class _HomePageState extends State<HomePage> {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "Feeles like ${forecastMap!["list"][0]["main"]["feels_like"]}°",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.black),
-                            ),
-                            Text(
-                              "${forecastMap!["list"][0]["weather"][0]["description"]}",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.black),
-                            ),
+                            Text("Feeles like ${forecastMap!["list"][0]["main"]["feels_like"]}°", style: const TextStyle(fontSize: 16, color: Colors.black),),
+                            Text("${forecastMap!["list"][0]["weather"][0]["description"]}", style: const TextStyle(fontSize: 16, color: Colors.black)),
                           ],
                         ),
                       ),
                       Center(
                         child: Column(
                           children: [
-                            Text(
-                              "Humidity :${forecastMap!["list"][0]["main"]["humidity"]}, Pressure ${forecastMap!["list"][0]["main"]["pressure"]}",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.black),
-                            ),
-                            Text(
-                              "Sunset ${Jiffy("${DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunset"] * 1000)}").format("h:mm:a")}, Sunrise ${Jiffy("${DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunrise"] * 1000)}").format("h:mm:a")}",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.black),
-                            ),
+                            Text("Humidity :${forecastMap!["list"][0]["main"]["humidity"]}, Pressure ${forecastMap!["list"][0]["main"]["pressure"]}", style: const TextStyle(fontSize: 16, color: Colors.black),),
+                            Text("Sunset ${Jiffy("${DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunset"] * 1000)}").format("h:mm:a")}, Sunrise ${Jiffy("${DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunrise"] * 1000)}").format("h:mm:a")}", style: const TextStyle(fontSize: 16, color: Colors.black),),
                           ],
                         ),
                       ),
-                      Container(
+                      SizedBox(
                         height: 200,
                         width: double.infinity,
                         child: ListView.builder(
@@ -106,41 +75,26 @@ class _HomePageState extends State<HomePage> {
                           itemBuilder: (context, index) {
                             return Container(
                               width: 90,
-                              margin: EdgeInsets.only(right: 8),
+                              margin: const EdgeInsets.only(right: 8),
                               height: double.infinity,
                               color: Colors.blueGrey,
                               child: Column(
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceAround,
                                   children: [
-                                    Text(
-                                      "${Jiffy(forecastMap!["list"][index]["dt_txt"]).format("EEE, h:mm")}",
-                                      style: TextStyle(
-                                          fontSize: 16, color: Colors.black),
-                                    ),
-                                    Image.network(
-                                      "https://th.bing.com/th/id/OIP.KOhzhOJQ4AHmN25RM2zFbAHaFP?pid=ImgDet&rs=1",
-                                      height: 70,
-                                      width: 71,
-                                    ),
-                                    Text(
-                                      "${forecastMap!["list"][index]["main"]["temp_min"]}/${forecastMap!["list"][index]["main"]["temp_max"]}",
-                                      style: TextStyle(
-                                          fontSize: 12, color: Colors.black),
-                                    ),
-                                    Text(
-                                      "${forecastMap!["list"][index]["weather"][0]["description"]}",
-                                      style: TextStyle(
-                                          fontSize: 12, color: Colors.black),
-                                    ),
-                                  ]),
+                                    Text(Jiffy(forecastMap!["list"][index]["dt_txt"]).format("EEE, h:mm"), style: const TextStyle(fontSize: 16, color: Colors.black),),
+                                    Image.network("https://th.bing.com/th/id/OIP.KOhzhOJQ4AHmN25RM2zFbAHaFP?pid=ImgDet&rs=1", height: 70, width: 71,),
+                                    Text("${forecastMap!["list"][index]["main"]["temp_min"]}/${forecastMap!["list"][index]["main"]["temp_max"]}", style: const TextStyle(fontSize: 12, color: Colors.black),),
+                                    Text("${forecastMap!["list"][index]["weather"][0]["description"]}", style: const TextStyle(fontSize: 12, color: Colors.black),),
+                                  ]
+                              ),
                             );
                           },
                         ),
                       )
                     ]),
               )
-            : Center(child: CircularProgressIndicator()),
+            : const Center(child: CircularProgressIndicator()),
       ),
     );
   }
@@ -150,20 +104,20 @@ class _HomePageState extends State<HomePage> {
     LocationPermission permission;
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
+       Future.error('Location services are disabled.');
     }
 
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+         Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
-      return Future.error(
+       Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
@@ -187,10 +141,10 @@ class _HomePageState extends State<HomePage> {
         "https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&units=metric&appid=cc93193086a048993d938d8583ede38a&fbclid=IwAR3Hr9_sSo-ju9Us4-W-MpsVaeQyp10SZvo84iTiJ7WjrqTNSkbxRURH5RQ"));
     setState(() {
       weatherMap = Map<String, dynamic>.from(jsonDecode(weatherResponce.body));
-      forecastMap =
-          Map<String, dynamic>.from(jsonDecode(forecastResponce.body));
+      forecastMap = Map<String, dynamic>.from(jsonDecode(forecastResponce.body));
     });
-    print("ssssssssssssssssssssss${weatherResponce.body}");
+    print("Weather Body:: ${weatherResponce.body}");
+    print("Weather Body:: ${forecastResponce.body}");
   }
 
   Map<String, dynamic>? weatherMap;
